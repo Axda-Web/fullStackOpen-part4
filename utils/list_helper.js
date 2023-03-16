ea7f8980a65a0fc616849801f97b61cc12749e13
Alyx DARENNE <alyx.darenne@deptagency.com>
2023-03-16 10:57:28 +0100

    part4/ex4: Add tests for the totalLikes helper function

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = blogs => {
+  return blogs.length ? blogs.map(blog => blog.likes).reduce((current, total) => current + total) : 0
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
