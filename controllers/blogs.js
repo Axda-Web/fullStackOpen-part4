6a54ba1bd9bab2b578f386066dc4c5e61f23a138
Alyx DARENNE <axda.web@gmail.com>
2023-04-27 10:52:17 +0200

    part4/ex19: Add token verification to POST new blog feature

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -13,7 +22,13 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: 'Invalid request data', message: 'Missing blog title or url' })
   }
 
-  const user = await User.findById(request.body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({ likes: 0, ...request.body, user: user.id })
 
