3fa9411fdfb849220910ffaf6f19dd286e925b01
Alyx DARENNE <axda.web@gmail.com>
2023-04-29 10:59:17 +0200

    part4/ex22: Refactor blog API tests to handle token auth

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
@@ -6,6 +8,16 @@ const tokenExtractor = (request, response, next) => {
     const token = authorization.replace('Bearer ', '')
     request.token = token
   }
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const userLoggedIn = await User.findById(decodedToken.id)
+  request.user = userLoggedIn
   next()
 }
 
@@ -29,7 +41,7 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   } else if (error.name ===  'JsonWebTokenError') {
-    return response.status(400).json({ error: error.message })
+    return response.status(401).json({ error: error.message })
   }
 
   next(error)
@@ -39,5 +51,6 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
-  tokenExtractor
+  tokenExtractor,
+  userExtractor
 }
